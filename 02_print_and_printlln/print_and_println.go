573cd75bb83677d5229d947150152716047fcce7
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2024-12-09 10:22:09 +0000

    8 print and println

--- 02_print_and_printlln/print_and_println.go
+++ 02_print_and_printlln/print_and_println.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    // fmt.Println("Hello, World!")
+	name := "Alice"
+	age := 25
+	height := 180.6
+
+	fmt.Println("nama:", name,"age:", age, "height:", height)
+
+	// printf  (print formatted)
+
+	fmt.Printf("name is %s\n",name) // \n print in new line
+	fmt.Printf("age is %d\n",age)
+	// fmt.Printf("height is %d cm\n",height) // this will give error
+	fmt.Printf("height is %.2f cm\n",height) // .2f means point ke baad 2 values
+
+	// to print the type of data type we use %T
+
+	fmt.Printf("Type of name is %T\n",name)
+	fmt.Printf("Type of age is %T\n",age)
+	fmt.Printf("Type of height is %T\n",height)
+
+	// %s, %d , %f are format specifiers
+
+	fmt.Printf("name is %s , age is %d , height is %f\n",name,age,height)
 }
