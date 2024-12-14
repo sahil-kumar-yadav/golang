a0faaf9782e471805748fcf9f6edbbeccfea3db7
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2024-12-14 09:21:50 +0000

    file reading using os package

--- 18_File_handling_1/main.go
+++ 18_File_handling_1/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	fmt.Println("Advance file handling")
+
+	// reading file through buffer
+	file, err := os.Open("./18_File_handling_1/sample.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close() // close the file when we're done
+
+	// create a buffer of size 1000
+	buffer := make([]byte, 1000)
+
+	// read file into buffer
+	n, err := file.Read(buffer)
+	if err != nil && err != io.EOF {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// print the content of the buffer
+	fmt.Printf("Read %d bytes: %s\n", n, string(buffer[:n]))
+	fmt.Println()
+
 }
